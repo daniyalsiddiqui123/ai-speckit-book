e3d608db2f1d87797ef15f0c993ca9f73a15cf04
daniyalsiddiqui123 <piratesdaniyal@gmail.com>
2025-12-08 13:54:50 +0500

    third commit

--- my-website/docusaurus.config.ts
+++ my-website/docusaurus.config.ts
@@ -18,7 +18,8 @@ const config: Config = {
   url: 'https://daniyalsiddiqui123.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/ai-speckit-book/',
+  baseUrl: '/',
+  trailingSlash: true,
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
